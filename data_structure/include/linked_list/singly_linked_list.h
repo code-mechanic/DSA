17a4a675459f1f35564c1134b8836bd25505f9c2
code-mechanic <hashincludefun.h@gmail.com>
2025-05-28 15:46:53 +0530

    feat: add readme and todos in linked list header

--- data_structure/include/linked_list/singly_linked_list.h
+++ data_structure/include/linked_list/singly_linked_list.h
@@ -18,6 +18,7 @@ typedef struct sll_t_ {
     uint32_t size;
 } sll_t;
 
+/* Basic operations */
 sll_t *sll_create(uint32_t data);
 sll_status_t sll_destroy(sll_t **list_ref);
 
@@ -30,7 +31,42 @@ sll_status_t sll_delete_at_back(sll_t **list_ref);
 sll_status_t sll_delete_at_index(sll_t **list_ref, uint32_t index);
 sll_status_t sll_delete_by_value(sll_t **list_ref, uint32_t data);
 
+/* TODO Traversal operations */
 sll_status_t sll_print(sll_t *head);
+/*
+Reverse the list - Iterative versions.
+print the list in reverse order - Use stack
+Count the number of nodes
+Check if list is empty
+Get nth node from beginning/end
+Find middle element of the list
+Detect and remove loop (cycle) - Use Floyd’s Cycle-Finding Algorithm (Tortoise & Hare).
+*/
+
+/* TODO Modification operations */
+/*
+Remove duplicates
+From unsorted list (use hash set).
+From sorted list (linear scan).
+Sort the linked list
+Merge sort (efficient for linked lists).
+Swap two nodes by value (not data)
+Rotate the list
+Rotate by k positions (left or right).
+Move last node to front
+Move last ‘k’ nodes to the front
+*/
+
+/* TODO Search and compare operations */
 uint32_t sll_search(sll_t *head, uint32_t data);
+
+/*
+Compare two lists
+    Element-wise check for equality.
+Check if a list is palindrome
+    Use fast/slow pointers + stack or reverse second half.
+Find intersection point of two linked lists
+Merge two sorted linked lists
+*/
 
 #endif /* SINGLY_LINKED_LIST_H */
---
+++ data_structure/readme.md
@@ -0,0 +1,14 @@
+# Data Structures
+
+
+To build the data structure
+
+`make all`
+
+To build test only
+
+`make test`
+
+To build problems only
+
+`make problems TARGET=<DATA_STRUCTURE>`
